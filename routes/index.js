74571c5a1537c77b5dd4277b308f6179f4dda684
jonathan rawlings <jonathanrawlings@jonathans-MBP.home>
2021-04-23 15:28:28 -0700

    added login page

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,16 @@ const router = express.Router()
 // @desc   Login /
 // @route GET /
 router.get('/', (req, res) => {
-    res.send('Login')
+    res.render('login', {
+        layout: 'login',
+    })
 })
 
 
 // @desc   Dashboard
 // @route  GET /dashboard
 router.get('/dashboard', (req, res) => {
-    res.send('Dashboard')
+    res.send('dashboard')
 })
 
 module.exports = router
