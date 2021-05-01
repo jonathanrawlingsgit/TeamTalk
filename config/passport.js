43b29cf7994929c2cef4328835d295e313999369
jonathan rawlings <jonathanrawlings@jonathans-MBP.home>
2021-05-01 09:20:40 -0700

    models/ routes

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,23 @@
 const GoogleStategy = require('passport-google-oauth20').Strategy
 const mongoose - require('mongoose')
+const User = require('../models/User')
+
+
+module.exports = function (passport) {
+    passport.use(
+        new GoogleStategy(
+            {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback'
+    }, async (accessToken, refreshToken, profile, done) => {
+        console.log(profile)
+    }))
+
+    passport.serializUser((user, done) => {
+        done(null, user.id)
+    })
+
+    passport.desrializeUser((id, done) => {
+        User.findById(id,(err, user) => done(err,user))})
+}
