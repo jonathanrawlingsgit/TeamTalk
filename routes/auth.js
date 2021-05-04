f60c79d8301736e31ae041fb697f7fa078b746b0
jonathan rawlings <jonathanrawlings@jonathans-MBP.home>
2021-05-04 14:29:10 -0700

    figured out the dashboard redirect problem

--- routes/auth.js
+++ routes/auth.js
@@ -9,9 +9,16 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 // @desc   Google auth callback
 // @route  GET /auth/google/callback
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: 
-'/'}), (req, res) => {
+router.get('/google/callback', passport.authenticate('google', 
+{ failureRedirect: '/'}), (req, res) => {
     res.redirect('/dashboard')
+})
+
+// @desc Logout user
+// @route /auth/logout
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
 })
 
 module.exports = router
